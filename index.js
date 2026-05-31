fb477050d9cc0c851880dfb7b1ee10efadc2fd2b
remiria1315 <remiria1315@outlook.jp>
2026-05-31 17:58:48 +0900

    upload

---
+++ index.js
@@ -0,0 +1,20 @@
+export default {
+  async fetch(request, env, ctx) {
+    // 現在時刻を取得して動的に表示する例
+    const now = new Date().toLocaleString("ja-JP");
+    
+    const html = `
+      <!DOCTYPE html>
+      <html>
+        <body>
+          <h1>動的サイトへようこそ！</h1>
+          <p>現在のサーバー時間は: ${now}</p>
+        </body>
+      </html>
+    `;
+
+    return new Response(html, {
+      headers: { "content-type": "text/html;charset=UTF-8" },
+    });
+  },
+};
